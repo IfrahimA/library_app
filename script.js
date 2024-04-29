3b076b4d6763ebf9da85ba56737303422cd14988
Ifrahim-WSL <147778089+IfrahimA@users.noreply.github.com>
2024-04-28 23:47:52 -0400

    Added Javascript, but autocomplete not working

---
+++ script.js
@@ -0,0 +1,20 @@
+clearBtn = document.getElementById("clear");
+addBookBtn = document.querySelector(".inital_display");
+
+
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.title = title; 
+    this.author = author; 
+    this.pages = pages; 
+    this.isRead = isRead; 
+}
+
+addBookBtn.
+
+addBookBtn.addEventListener('click', () => 
+{
+    addBookBtn.remove();
+    
+});
