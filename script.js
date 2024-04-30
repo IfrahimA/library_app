9e1858c6f0fc9011ce4a52f0a76f0fcc6d922eac
Ifrahim-WSL <147778089+IfrahimA@users.noreply.github.com>
2024-04-30 19:44:07 -0400

    Attempting to add an interactive read button

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-clearBtn = document.getElementById("clear");
-addBookBtn = document.querySelector(".inital_display");
-books = document.querySelector(".books");
-modal = document.querySelector(".modal");
-submitBookBtn = document.querySelector("#submit");
-inputTitle = document.getElementById("input_Title");
-inputAuthor = document.getElementById("input_Author");
-inputPages = document.getElementById("input_Pages");
-inputRead = document.getElementById("read");
+const clearBtn = document.getElementById("clear");
+const addBookBtn = document.querySelector(".inital_display");
+const books = document.querySelector(".books");
+const modal = document.querySelector(".modal");
+const submitBookBtn = document.querySelector("#submit");
+const inputTitle = document.getElementById("input_Title");
+const inputAuthor = document.getElementById("input_Author");
+const inputPages = document.getElementById("input_Pages");
 let newBook; 
 
 const myLibrary = [];
@@ -21,31 +20,89 @@ function Book(title, author, pages, isRead) {
 function addBook()
 {
     addBookBtn.addEventListener('click', () => {
-        addBookBtn.style.display = 'none';
         modal.style.display = "block";
+        books.style.display = "grid";
     });
 }
 
-/* Create a function to clear everything, back to the beginning.*/
-function clear()
-{
 
-}
+isReadBtn = document.createElement("button");
+removeBtn = document.createElement("button");
 
 function submit()
-{
+{    
     submitBookBtn.addEventListener('click', (e) => {
         e.preventDefault();
-        console.log(inputTitle.value);
-        console.log(inputAuthor.value);
-        console.log(inputPages.value);
-        console.log(inputRead.value);
-        const newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.value);
+        const isChecked = document.getElementById('read').checked;
+        const newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, isChecked);
         myLibrary.push(newBook);
-        modal.style.display = "none"; 
-        addBookBtn.style.display = "grid"; 
+        modal.style.display = "none";
+
+        
+        const displayBook = document.createElement('div'); 
+        books.appendChild(displayBook);
+        displayBook.classList.add("book");
+
+
+        /* Adding Properties of Each Block */
+        title = document.createElement("p");
+        author = document.createElement("p");
+        pages = document.createElement("p");
+        title.classList.add("card-text");
+        author.classList.add("card-text");
+        pages.classList.add("card-text");
+        displayBook.appendChild(title);
+        displayBook.appendChild(author);
+        displayBook.appendChild(pages);
+        title.textContent = newBook.title;
+        author.textContent = newBook.author; 
+        pages.textContent = newBook.pages + " pages"; 
+  
+
+        /* Adding Buttons */
+        removeBtn.textContent = "Remove"
+        if (newBook.isRead == false)
+        {
+            isReadBtn.classList.add("not-read");
+            isReadBtn.textContent = "Not Read!";
+        }
+        else
+        {
+            isReadBtn.classList.add("is-read"); 
+            isReadBtn.textContent = "Read"; 
+        }
+        removeBtn.classList.add("remove-button");
+        displayBook.appendChild(isReadBtn); 
+        displayBook.appendChild(removeBtn);
     });
+}
+
+isReadBtn.addEventListener('click', () => {
+    if (isReadBtn.textContent == "Not Read") {
+        isReadBtn.classList.remove("not-read");
+        isReadBtn.classList.add("is-read");
+        isReadBtn.textContent = "Read";
+    }
+    else if (isReadBtn.textContent == "Read") {
+        isReadBtn.classList.remove("is-read");
+        isReadBtn.classList.add("not-read");
+        isReadBtn.textContent = "Not Read";
+    }
+})
+
+function removeBook()
+{
+
+}
+
+function clear()
+{
+    clearBtn.addEventListener('click', () => 
+    {
+        books.replaceChildren();
+    })
 }
 
 addBook();
 submit();
+clear(); 
